d03608b4d8a4738f3effb6f01002b10d07b1348f
Mohammad Shahrukh <msr.concordfly@gmail.com>
2017-02-15 23:39:13 +0530

    Footer finalized

--- social/views.py
+++ social/views.py
@@ -140,7 +140,7 @@ def search(request) :
 		Q(first_name__icontains=query)|
 		Q(last_name__icontains=query)|
 		Q(email__icontains=query)
-		)[:2]
+		)[:6]
 	
 	searchusers = []
 	for i in peoplequery :
